8328adaaf0a26cf86b912f6adc6a2dea1cecd98f
Richard McSharry <Richard McSharry>
2017-03-02 16:17:23 +0100

    Adds model test to check car has unique name

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,14 +3,6 @@ class Car < ApplicationRecord
   
   friendly_id :name, use: :slugged
 
-  # before_validation :set_a_slug
-  
-  # def set_a_slug
-  #   if self.slug.nil?
-  #     self.slug = SecureRandom.urlsafe_base64(5)
-  #   end
-  # end
-
   validates_presence_of :name
   validates_uniqueness_of :name
 end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(
+    "name": "Suburu Imprezza",
+    "max_speed": "120 km/h",
+    )
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with non-unique name" do
+    subject.save
+    dup_subject = described_class.new("name": "Suburu Imprezza")
+    dup_subject.save
+    expect(dup_subject).to_not be_valid
+  end
 end
