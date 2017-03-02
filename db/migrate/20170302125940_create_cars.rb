8bdab876ac45af0b264acd18bf552b2528b32b76
Richard McSharry <Richard McSharry>
2017-03-02 14:49:58 +0100

    Fixes car migration, adds unique index for slug, name

--- db/migrate/20170302125940_create_cars.rb
+++ db/migrate/20170302125940_create_cars.rb
@@ -1,12 +1,14 @@
 class CreateCars < ActiveRecord::Migration[5.0]
   def change
     create_table :cars do |t|
-      t.string :name
-      t.string :car_slug
+      t.string :name, null: false
+      t.string :car_slug, null: false
       t.string :max_speed
       t.string :max_speed_on_track
 
       t.timestamps
     end
+    add_index :cars, :name, :unique => true
+    add_index :cars, :car_slug, :unique => true
   end
 end
