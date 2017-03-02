bbcd8582a6a20f6f45ec63ed64619c8d14433ccc
Richard McSharry <Richard McSharry>
2017-03-02 23:23:01 +0100

    Adds failing tests for validity of track & max_speed_on_track

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -57,7 +57,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-   config.before(:suite) do
+  config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean_with(:truncation)
   end
--- spec/requests/cars_spec.rb
+++ spec/requests/cars_spec.rb
@@ -7,19 +7,28 @@ RSpec.describe "Cars", type: :request do
       get api_v1_cars_path
       expect(response).to have_http_status(200)
     end
+
+    it "does not include attribute max_speed_on_track in response" do
+      Car.create!(name:'Suburu Impreza', max_speed:'120km/h')
+      get api_v1_cars_path
+      car = json(response.body).first
+      expect(car).not_to include_json(car: {max_speed_on_track: nil})      
+    end
   end
 
   describe "GET api/v1/cars/:id" do
-    it "includes attribute max_speed_on_track as nil value" do
-      Car.create!(name:'Suburu Impreza', max_speed:'120km/h')
-      get api_v1_car_path(id: 'suburu-impreza')
-
-      # car = json(response.body)[:car]            
-      # msot = car[:max_speed_on_track]
-      
+    it "max_speed_on_track = 'no track selected' when no track provided" do
+      Car.create!(name:'Suburu Impreza', max_speed:'120km/h', track: nil)
+      get api_v1_car_path(id: 'suburu-impreza')      
       expect(response).to have_http_status(200) 
-      expect(response.body).to include_json(car: {max_speed_on_track: nil})
-      
+      expect(response.body).to include_json(car: {max_speed_on_track: 'no track selected'})
     end
+
+    it "max_speed_on_track = 'track not found' when non-existent track provided" do
+      Car.create!(name:'Suburu Impreza', max_speed:'120km/h', track: 'test')
+      get api_v1_car_path(id: 'suburu-impreza')      
+      expect(response).to have_http_status(200) 
+      expect(response.body).to include_json(car: {max_speed_on_track: 'track not found'})
+    end    
   end
 end
